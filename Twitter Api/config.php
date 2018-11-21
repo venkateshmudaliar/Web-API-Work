ef6a8c390726b2eb7d953b3ba0fc7285d0dfdf3e
Venkatesh Mudaliar <venkateshmudaliar08@gmail.com>
2018-11-21 22:09:22 +0530

    Add files via upload

---
+++ Twitter Api/config.php
@@ -0,0 +1,6 @@
+<?php
+//Replace XXX with your twitter application variables.
+define('CONSUMER_KEY', 'nKt7yxgzTQM863AxXPQEl5QqL');
+define('CONSUMER_SECRET', 'OaOkMtM0h0YlHyO8ilz8BC2nw8M6NNWxgL5AdUxWgSlHj38UGq');
+define('OAUTH_CALLBACK', 'http://localvenky.com/combo/process.php');
+?>
